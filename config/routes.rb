4e27ec760c4a0130212dcf65fe617519b2fac3f9
Tilak <patidartilak100@gmail.com>
2023-11-19 14:25:10 +0530

    another CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   root to: 'pages#home'
-  resources :quotes
+  resources :quotes do
+    resources :line_item_dates, except: %i[index show]
+  end
 end
